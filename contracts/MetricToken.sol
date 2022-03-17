1c6cd223ed934752279680d6edcd0877d59fb03b
gnomadic <edflem+github@gmail.com>
2022-03-17 14:03:05 -0400

    setting token name, symbol, and 1B mint on METRIC

--- contracts/MetricToken.sol
+++ contracts/MetricToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @custom:security-contact metric@gmail.com
 contract MetricToken is ERC20 {
-    constructor() ERC20("METRIC", "MTR") {
-        _mint(msg.sender, 1000000 * 10**decimals());
+    constructor() ERC20("METRIC", "METRIC") {
+        _mint(msg.sender, 1000000000 * 10**decimals());
     }
 }
