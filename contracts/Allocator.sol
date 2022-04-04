0a003e6c88a48fbc655518ac42155d999fb2548b
gnomadic <edflem+github@gmail.com>
2022-04-03 20:45:54 -0400

    first draft allocation

--- contracts/Allocator.sol
+++ contracts/Allocator.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./MetricToken.sol";
 
 contract Allocator is AccessControl {
+    using SafeMath for uint256;
+
     bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
 
+    uint256 private MOONDUST_PER_BLOCK = 4 * 10**18;
+    uint256 private constant ACC_DUST_PRECISION = 1e12;
+
     AllocationGroup[] private _allocations;
+    uint256 private totalAllocPoint;
+
     MetricToken private _metric;
 
     constructor(MetricToken metric) {
@@ -24,6 +32,7 @@ contract Allocator is AccessControl {
     ) external onlyRole(DISTRIBUTOR_ROLE) {
         AllocationGroup memory group = AllocationGroup(toAdd, shares, distribution, block.number);
         _allocations.push(group);
+        totalAllocPoint = totalAllocPoint.add(group.allocPoint);
     }
 
     function getAllocationGroups() public view returns (AllocationGroup[] memory) {
@@ -32,17 +41,33 @@ contract Allocator is AccessControl {
 
     function removeAllocationGroup(uint8 index) external onlyRole(DISTRIBUTOR_ROLE) {
         require(index < _allocations.length);
+        totalAllocPoint = totalAllocPoint.sub(_allocations[index]);
+
         _allocations[index] = _allocations[_allocations.length - 1];
         _allocations.pop();
     }
 
-    function getTotalAllocatedShares() public view returns (uint8) {
-        uint8 total = 0;
-        uint256 count = _allocations.length;
-        for (uint8 i = 0; i < count; i++) {
-            total += _allocations[i]._shares;
+    function getTotalAllocationPoints() public view returns (uint256) {
+        return totalAllocPoint;
+    }
+
+    function updateAllocations(uint256 _agIndex) public {
+        AllocationGroup storage group = _allocations[_agIndex];
+
+        if (block.number <= group._lastRewardBlock) {
+            return;
         }
-        return total;
+
+        // TODO confirm budget is correct with assertions
+
+        uint256 blocks = block.number.sub(group.lastRewardBlock);
+
+        uint256 eggReward = blocks.mul(MOONDUST_PER_BLOCK).div(totalAllocPoint);
+
+        _metric.transfer(group._address, eggReward);
+
+        group.accMoonPerShare = group.accMoonPerShare.add(eggReward.mul(ACC_DUST_PRECISION);
+        group.lastRewardBlock = block.number;
     }
 
     struct AllocationGroup {
@@ -50,5 +75,7 @@ contract Allocator is AccessControl {
         uint8 _shares;
         bool _autodistribute;
         uint256 _lastRewardBlock;
+        uint256 rewardDebt; // Reward Debt is modelled after Sushi's MasterChefv2
+        uint256 accMoonPerShare;
     }
 }
