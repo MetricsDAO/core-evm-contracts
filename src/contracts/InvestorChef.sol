6e687732bd9bcdb5f075ad0398e49ffcb6f34073
addyxvii <addisonbennett@gmail.com>
2022-06-24 13:06:10 -0600

    update investor chef

--- src/contracts/InvestorChef.sol
+++ src/contracts/InvestorChef.sol
@@ -1,24 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./TopChef.sol";
 
 contract InvestorChef is TopChef {
-    address public groupAddress;
-    uint256 public agIndex;
-    uint256 public shares;
-    bool public newAutoDistribute;
-
-    constructor(address metricTokenAddress) TopChef(metricTokenAddress) {
-        setMetricToken(metricTokenAddress);
-        setMetricPerBlock(4);
-        toggleRewards(false); // locking contract initially
-        updateAllocationGroup(groupAddress, agIndex, shares, newAutoDistribute);
-        viewPendingHarvest(agIndex);
-        viewPendingClaims(agIndex);
-        harvestAll();
-        harvest(agIndex);
-        claim(agIndex);
-    }
+    constructor(address metricTokenAddress) TopChef(metricTokenAddress) {}
 }
