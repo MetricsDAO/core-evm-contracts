48590637cae10b695d4e0e4a94b6b86c27a6122e
gnomadic <edflem+github@gmail.com>
2022-06-25 10:50:54 -0400

    fixing tests and standardizing on 0.8.13

--- src/contracts/Question/interfaces/IQuestionStateController.sol
+++ src/contracts/Question/interfaces/IQuestionStateController.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
 interface IQuestionStateController {
     function initializeQuestion(uint256 questionId) external;
