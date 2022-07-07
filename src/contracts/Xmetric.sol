b3f80863f16d683a9c06fbbec3c5a2ec78f3b84a
James Daly <jaimiedaly@gmail.com>
2022-07-07 13:48:02 -0500

    add import

--- src/contracts/Xmetric.sol
+++ src/contracts/Xmetric.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 contract Xmetric is ERC20("xMETRIC", "xMETRIC"), ERC20Pausable, Ownable {
     constructor() {
