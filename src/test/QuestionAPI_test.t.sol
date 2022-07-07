442ce196a964093168e501871db7ef6eaa8616f0
addyxvii <addisonbennett@gmail.com>
2022-07-07 14:00:19 -0600

    complete test for disqualifying a question

--- src/test/QuestionAPI_test.t.sol
+++ src/test/QuestionAPI_test.t.sol
@@ -31,16 +31,11 @@ contract QuestionAPITest is Test {
         vm.label(alice, "Alice");
 
         vm.startPrank(owner);
-        //Deploy Metric
+        //Deploy Metric and controllers
         _metricToken = new MetricToken();
         vm.label(address(_metricToken), "METRIC");
-        //Fund Accounts
-        _metricToken.transfer(owner, 10000e18);
-
-        //Approve Metric Transfer
-        _metricToken.approve(address(_actionCostController), _metricToken.balanceOf(owner));
-
         _actionCostController = new ActionCostController(address(_metricToken));
+        _metricToken.approve(address(_actionCostController), _metricToken.balanceOf(owner));
         _bountyQuestion = new BountyQuestion();
         _claimController = new ClaimController();
         _questionStateController = new QuestionStateController();
@@ -51,21 +46,35 @@ contract QuestionAPITest is Test {
             address(_claimController),
             address(_actionCostController)
         );
+        //fund questionApi so it can pay for question creation TODO: this needs to change
+        _metricToken.transfer(address(_questionAPI), 100e18);
+        vm.stopPrank();
+
+        vm.startPrank(address(_questionAPI));
+        _metricToken.approve(address(_actionCostController), _metricToken.balanceOf(address(_questionAPI)));
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+        //transfer ownership so that onlyowner functions can be called
+        _actionCostController.transferOwnership(address(_questionAPI));
+        _questionStateController.transferOwnership(address(_questionAPI));
+        _claimController.transferOwnership(address(_questionAPI));
+        _bountyQuestion.transferOwnership(address(_questionAPI));
         vm.stopPrank();
     }
 
     function testInitialMint() public {
-        // assertTrue(_metricToken.balanceOf(address(_vestingContract)) == 1000000000 * 10**18);
+        //     // assertTrue(_metricToken.balanceOf(address(_vestingContract)) == 1000000000 * 10**18);
     }
+
+    //TODO change the caller for payForCreation from questionAPI
 
     function testdisqualifyQuestion() public {
         vm.startPrank(owner);
-        _actionCostController.payForCreateQuestion();
         badQuestion = _questionAPI.createQuestion("Bad question", 1);
         _questionAPI.disqualifyQuestion(badQuestion);
         questionState = _questionStateController.getState(badQuestion);
-        //assert badQuestion state is BAD not sure what the value for question state should be yet
-        assertEq(questionState, 5);
+        assertEq(questionState, 7);
         vm.stopPrank();
     }
 }
