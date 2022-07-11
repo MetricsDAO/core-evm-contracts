3eee49e3f7acb9909d38389c43e79b6c25f5091b
gnomadic <edflem+github@gmail.com>
2022-07-11 14:47:49 -0400

    adding pause and unpause

---
+++ scripts/xMetric/Pause.ts
@@ -0,0 +1,24 @@
+import { ethers } from "hardhat";
+import fs from "fs";
+
+async function main() {
+  const network = process.env.HARDHAT_NETWORK;
+  console.log("Pausing on: ", network);
+
+  const data = await fs.readFileSync("./deployments/" + network + "/Xmetric.json", "utf8");
+  const object = JSON.parse(data);
+  const address = object.address;
+
+  const MyContract = await ethers.getContractFactory("Xmetric");
+  const contract = await MyContract.attach(address);
+
+  const tx = await contract.pause();
+  await tx.wait();
+
+  console.log("done");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
