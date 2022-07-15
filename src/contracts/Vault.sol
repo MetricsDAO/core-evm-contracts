1bfd504b20300d6f76bd2dd9158e7d7fdeff0f5b
addyxvii <addisonbennett@gmail.com>
2022-07-15 10:57:13 -0600

    remove unneeded vars

--- src/contracts/Vault.sol
+++ src/contracts/Vault.sol
@@ -6,41 +6,27 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vault is Ownable {
-    struct Items {
-        IERC20 metric;
-        address withdrawer;
-        uint256 amount;
-        uint256 unlockTimestamp;
-        bool withdrawn;
-        bool deposited;
-    }
-
     uint256 public depositsCount;
-    mapping(address => uint256[]) public depositsByMetricAddress;
     mapping(address => uint256[]) public depositsByWithdrawers;
     mapping(uint256 => Items) public lockedMetric;
     mapping(address => mapping(address => uint256)) public walletMetricBalance;
 
-    event Withdraw(address withdrawer, uint256 amount);
-
-    constructor() {}
+    constructor(address metricTokenAddress) {
+        setMetricToken(metricTokenAddress);
+    }
 
     function lockMetric(
-        IERC20 _metric,
         address _withdrawer,
         uint256 _amount,
         uint256 _unlockTimestamp
     ) external returns (uint256 _id) {
-        require(_amount >= 500, "Metric amount too low!");
         require(_unlockTimestamp < 10000000000, "Unlock timestamp is not in seconds!");
         require(_unlockTimestamp > block.timestamp, "Unlock timestamp is not in the future!");
-        require(_metric.allowance(msg.sender, address(this)) >= _amount, "Approve tokens first!");
         _metric.safeTransferFrom(msg.sender, address(this), _amount);
 
         walletMetricBalance[address(_metric)][msg.sender] = walletMetricBalance[address(_metric)][msg.sender].add(_amount);
 
         _id = ++depositsCount;
-        lockedMetric[_id].metric = _metric;
         lockedMetric[_id].withdrawer = _withdrawer;
         lockedMetric[_id].amount = _amount;
         lockedMetric[_id].unlockTimestamp = _unlockTimestamp;
@@ -52,11 +38,11 @@ contract Vault is Ownable {
         return _id;
     }
 
-    function withdrawmetrics(uint256 _id) external {
+    function withdrawMetric(uint256 _id) external {
         require(block.timestamp >= lockedMetric[_id].unlockTimestamp, "Metrics are still locked!");
         require(msg.sender == lockedMetric[_id].withdrawer, "You are not the withdrawer!");
-        require(lockedMetric[_id].deposited, "Metrics are not yet deposited!");
-        require(!lockedMetric[_id].withdrawn, "Metrics are already withdrawn!");
+        require(lockedMetric[_id].deposited, "Metric is not yet deposited!");
+        require(!lockedMetric[_id].withdrawn, "Metric already withdrawn!");
 
         lockedMetric[_id].withdrawn = true;
 
@@ -86,5 +72,15 @@ contract Vault is Ownable {
 
     function getMetricTotalLockedBalance(address _metric) external view returns (uint256) {
         return IERC20(_metric).balanceOf(address(this));
+    }
+
+    event Withdraw(address withdrawer, uint256 amount);
+
+    struct Items {
+        address withdrawer;
+        uint256 amount;
+        uint256 unlockTimestamp;
+        bool withdrawn;
+        bool deposited;
     }
 }
