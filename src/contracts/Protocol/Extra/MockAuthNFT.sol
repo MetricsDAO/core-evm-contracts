ab98893d9fd55128effba8b78512f6fc16a6a99e
thermohaline <>
2022-08-04 12:23:08 +0200

    Update permissions required for publishing questions

--- src/contracts/Protocol/Extra/MockAuthNFT.sol
+++ src/contracts/Protocol/Extra/MockAuthNFT.sol
@@ -2,13 +2,14 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
+contract NFT is ERC721, Ownable {
     uint256 public currentTokenId;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
-    function mintTo(address recipient) public payable returns (uint256) {
+    function mintTo(address recipient) public payable onlyOwner returns (uint256) {
         uint256 newItemId = ++currentTokenId;
         _safeMint(recipient, newItemId);
         return newItemId;
