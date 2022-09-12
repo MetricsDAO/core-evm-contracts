2e60a6f47199de081d16c717e973ae0ee2c4d2aa
thermohaline <36921585+thermohaline@users.noreply.github.com>
2022-09-12 17:49:07 +0200

    Fix tests that break because of threshold -- still need elegant solution later on

--- src/contracts/Protocol/modifiers/TokenBar.sol
+++ src/contracts/Protocol/modifiers/TokenBar.sol
@@ -6,15 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // Interfaces
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "../interfaces/IClaimController.sol";
+import "../interfaces/IQuestionAPI.sol";
 
 abstract contract TokenBar is Ownable {
     address public metricToken;
+    IQuestionAPI public questionAPI;
 
     /// @notice Throw if user tries to claim with balance lower than threshold
     error NotEnoughTokens();
 
-    function setMetricToken(address _metricToken) public onlyOwner {
-        metricToken = _metricToken;
+    function updateMetric() public {
+        metricToken = questionAPI.getMetricToken();
+    }
+
+    function setQuestionApiMT(address _questionAPI) public onlyOwner {
+        questionAPI = IQuestionAPI(_questionAPI);
     }
 
     modifier tokenThreshold(address user, uint256 questionId) {
